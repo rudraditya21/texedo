60bfd22676789c395d27e87af59d13755fa24908
Rudraditya Thakur <rudraditya.thakur21@gmail.com>
2025-12-29 17:07:13 +0530

    added: settings dialog, updated: header, added: dialog component

--- www/components/header.tsx
+++ www/components/header.tsx
@@ -1,12 +1,35 @@
 import { ThemeToggle } from "./theme-toggle";
 import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { SettingsIcon } from "lucide-react";
 
 export default function Header() {
   return (
     <div className="py-4 px-6 flex items-center justify-between border-b">
       <h1 className="tracking-wider">TEXEDO</h1>
       <div className="flex items-center space-x-4">
-        <Button variant="outline" className="text-xs uppercase rounded-full">Settings</Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" className="text-xs uppercase rounded-full flex items-center ">
+              <SettingsIcon />
+              <span>Settings</span>
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="max-w-5xl">
+            <DialogHeader>
+              <DialogTitle>Settings</DialogTitle>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
         <ThemeToggle />
       </div>
     </div>
